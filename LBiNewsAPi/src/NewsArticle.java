7af147ab8cd8f6e54904f7de50890c897964d851
Nicholas Nicos Stamatakis <116750704+nick-stamatakis@users.noreply.github.com>
2023-04-14 11:50:28 -0400

    Add files via upload

---
+++ LBiNewsAPi/src/NewsArticle.java
@@ -0,0 +1,10 @@
+public class NewsArticle {
+    private String title;
+    private String description;
+    private String url;
+    private String author;
+    private String publishedAt;
+    // constructors, getters, and setters
+    
+}
+
